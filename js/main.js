7998b4905f098215800820796c85f0c665302112
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-03-19 00:19:31 +0530

    ...

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const themeIcon = themeToggle.querySelector('i');
     const themeText = themeToggle.querySelector('span');
     const color = document.getElementById("brand-color");
+    const cardTitle = document.querySelectorAll(".card-title");
+    const cardText = document.querySelectorAll(".card-text");
+    const viewProjectbtn = document.querySelectorAll(".btnViewProject");
     let isDarkMode = false;
 
     // Check for saved theme preference
@@ -24,16 +27,38 @@ document.addEventListener('DOMContentLoaded', function () {
             themeIcon.classList.replace('fa-sun', 'fa-moon');
             themeText.textContent = 'Dark';
             localStorage.setItem('theme', 'dark');
-
             color.classList.replace('text-primary','text-warning');
+
+            for (let i = 0; i < cardTitle.length; i++) {
+                cardTitle[i].classList.replace('text-dark','text-light');
+            }
+
+            for (let i = 0; i < cardText.length; i++) {
+                cardText[i].classList.replace('text-dark','text-light');
+            }
+
+            for (let i = 0; i < viewProjectbtn.length; i++) {
+                viewProjectbtn[i].classList.replace('btn-outline-dark','btn-outline-warning');
+            }
             
         } else {
             document.body.setAttribute('data-theme', 'light');
             themeIcon.classList.replace('fa-moon', 'fa-sun');
             themeText.textContent = 'Light';
             localStorage.setItem('theme', 'light');
+            color.classList.replace('text-warning','text-primary');
 
-            color.classList.replace('text-warning','text-primary');            
+            for (let i = 0; i < cardTitle.length; i++) {
+                cardTitle[i].classList.replace('text-light','text-dark');
+            }
+
+            for (let i = 0; i < cardText.length; i++) {
+                cardText[i].classList.replace('text-light','text-dark');
+            }
+
+            for (let i = 0; i < viewProjectbtn.length; i++) {
+                viewProjectbtn[i].classList.replace('btn-outline-warning','btn-outline-dark');
+            }
         }
     });
 });
