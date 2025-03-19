fd43c9be1cefd9b020d92483dbf434833edcd676
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-03-19 09:33:06 +0530

    ----

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const themeIcon = themeToggle.querySelector('i');
     const themeText = themeToggle.querySelector('span');
     const color = document.getElementById("brand-color");
-    const cardTitle = document.getElementsByClassName("card-title");
-    const cardText = document.getElementsByClassName("card-text");
+    const mode = document.getElementsByClassName("mode-changer");
+    const role = document.getElementsByClassName("testimonial-role");
     const viewProjectbtn = document.getElementsByClassName("btnViewProject");
+    const container = document.getElementById("about-container");
     let isDarkMode = false;
 
     // Check for saved theme preference
@@ -27,38 +28,47 @@ document.addEventListener('DOMContentLoaded', function () {
             themeIcon.classList.replace('fa-sun', 'fa-moon');
             themeText.textContent = 'Dark';
             localStorage.setItem('theme', 'dark');
-            color.classList.replace('text-primary','text-warning');
+            color.classList.replace('text-primary', 'text-warning');
 
-            for (let i = 0; i < cardTitle.length; i++) {
-                cardTitle[i].classList.replace('text-dark','text-light');
+            container.classList.remove('bg-body-tertiary');
+
+            for (let i = 0; i < mode.length; i++) {
+                mode[i].classList.replace('text-dark', 'text-light');
             }
 
-            for (let i = 0; i < cardText.length; i++) {
-                cardText[i].classList.replace('text-dark','text-light');
+            for (let i = 0; i < role.length; i++) {
+                role[i].classList.replace('text-muted', 'text-light');
             }
 
             for (let i = 0; i < viewProjectbtn.length; i++) {
-                viewProjectbtn[i].classList.replace('btn-outline-dark','btn-outline-warning');
+                viewProjectbtn[i].classList.replace('btn-outline-dark', 'btn-outline-warning');
             }
-            
+
+            document.body.classList.toggle("dark-mode");
+
+
         } else {
             document.body.setAttribute('data-theme', 'light');
             themeIcon.classList.replace('fa-moon', 'fa-sun');
             themeText.textContent = 'Light';
             localStorage.setItem('theme', 'light');
-            color.classList.replace('text-warning','text-primary');
+            color.classList.replace('text-warning', 'text-primary');
 
-            for (let i = 0; i < cardTitle.length; i++) {
-                cardTitle[i].classList.replace('text-light','text-dark');
+            container.classList.add('bg-body-tertiary');
+
+            for (let i = 0; i < mode.length; i++) {
+                mode[i].classList.replace('text-light', 'text-dark');
             }
 
-            for (let i = 0; i < cardText.length; i++) {
-                cardText[i].classList.replace('text-light','text-dark');
+            for (let i = 0; i < role.length; i++) {
+                role[i].classList.replace('text-light', 'text-muted');
             }
 
             for (let i = 0; i < viewProjectbtn.length; i++) {
-                viewProjectbtn[i].classList.replace('btn-outline-warning','btn-outline-dark');
+                viewProjectbtn[i].classList.replace('btn-outline-warning', 'btn-outline-dark');
             }
+
+            document.body.classList.toggle("dark-mode");
         }
     });
 });
